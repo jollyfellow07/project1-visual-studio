535b619c6de000f140494b744a74ae743bb7c757
jollyfellow7 <simonecompagnone7@gmail.com>
2022-03-31 11:58:33 +0200

    Add project files.

---
+++ project1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Aggiungi un nome: ");
+
+var testo = Console.ReadLine();
+
+Console.WriteLine(testo);
